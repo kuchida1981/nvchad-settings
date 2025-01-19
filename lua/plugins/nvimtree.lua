1da690d5025c0df173be6f0f6c54b21b55cc7e00
Kosuke Uchida <byebyeearthjpn@gmail.com>
2025-01-19 14:51:03 +0900

    nvim-tree configs fixed

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,9 +1,8 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    opts = vim.tbl_extend("force", require "nvchad.configs.nvimtree", {
+    opts = {
       filters = { dotfiles = true },
-    }),
+    },
   },
 }
