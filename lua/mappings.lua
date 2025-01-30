2ab1ce376126a58ba4750ca6042e3271ccb970cb
Kosuke Uchida <byebyeearthjpn@gmail.com>
2025-01-31 00:29:40 +0900

    telescope-live-grep-args added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,3 +5,6 @@ local nomap = vim.keymap.del
 
 map("i", "jk", "<ESC>")
 nomap("i", "<C-h>")
+
+-- telescope-live-grep-args.nvim
+map("n", "<leader>fg", "<cmd>Telescope live_grep_args<CR>", { desc = "telescope live grep args" })
---
+++ lua/plugins/telescope-live-grep-args.lua
@@ -0,0 +1,12 @@
+---@type NvPluginSpec[]
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        version = "^1.0.0",
+      },
+    },
+  },
+}
