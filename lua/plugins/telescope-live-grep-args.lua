235ec874e52cbc6970d796111175dd9db3a6c134
Kosuke Uchida <byebyeearthjpn@gmail.com>
2025-01-31 01:21:16 +0900

    fixed

--- lua/plugins/telescope-live-grep-args.lua
+++ lua/plugins/telescope-live-grep-args.lua
@@ -8,5 +8,18 @@ return {
         version = "^1.0.0",
       },
     },
+    opts = function(_, conf)
+      local lga_actions = require "telescope-live-grep-args.actions"
+      conf.extensions.live_grep_args = {
+        auto_quoting = true,
+        mappings = {
+          i = {
+            ["<C-k>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob" }),
+          },
+        },
+      }
+      return conf
+    end,
   },
 }
