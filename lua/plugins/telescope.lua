1810e7eb6c0a67a7226245b82d1f6f50c96a2daa
kuchida1981 <byebyeearthjpn@gmail.com>
2025-01-31 01:44:29 +0900

    telescope-live-grep-args added (#14)
    * telescope-live-grep-args added
    
    * fixed
    
    * fixed

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+---@type NvPluginSpec[]
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        version = "^1.0.0",
+      },
+    },
+    opts = function(_, conf)
+      local lga_actions = require "telescope-live-grep-args.actions"
+      conf.extensions.live_grep_args = {
+        auto_quoting = true,
+        mappings = {
+          i = {
+            ["<C-k>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob" }),
+          },
+        },
+      }
+      return conf
+    end,
+  },
+}
