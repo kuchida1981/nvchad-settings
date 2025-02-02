41557d94f4f904ffb8086a6cbefafb648cc47540
kuchida1981 <byebyeearthjpn@gmail.com>
2025-02-02 17:43:54 +0900

    telescope-frecency installed (#15)
    * telescope-frecency installed
    
    * fixed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,15 +7,24 @@ return {
         "nvim-telescope/telescope-live-grep-args.nvim",
         version = "^1.0.0",
       },
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        version = "*",
+        config = function()
+          require("telescope").load_extension "frecency"
+        end,
+      },
     },
     opts = function(_, conf)
       local lga_actions = require "telescope-live-grep-args.actions"
-      conf.extensions.live_grep_args = {
-        auto_quoting = true,
-        mappings = {
-          i = {
-            ["<C-k>"] = lga_actions.quote_prompt(),
-            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob" }),
+      conf.extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt { postfix = " --iglob" },
+            },
           },
         },
       }
